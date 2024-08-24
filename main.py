23b087125aff25684dcbb7290961c6c09e710139
GecielBorges <geciel@midiaroots.com.br>
2024-08-24 16:25:26 -0300

    Nova_Configuração
    Configuração inicial 02
    
    #Para rodar o FastAPI com um servidor ASGI (como o uvicorn),
    #você também pode instalar o seguinte:
    # pip install uvicorn
    #ver diretório em que vc está pwd
    #uvicorn main:app --reload

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+#FastAPI é um biblioteca que permite criar APIs de forma rápida e simples. 
+# API: interface de comunicação entre plataformas.  
 from fastapi import FastAPI
-randon import
+import random  # Importação correta do módulo random
 
 app = FastAPI()
 
-@app.get("/helloword")
+@app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/funcaoteste")
+@app.get("/teste1")
 async def funcaoteste():
-    return {"Hello": True, "num_aleatorio": randon.randint(a: 0, B:1000)}
+    return {"Hello": True, "num_aleatorio": random.randint(0, 1000)}  # Correção na chamada do randint
+
+
+#Para rodar o FastAPI com um servidor ASGI (como o uvicorn),
+#você também pode instalar o seguinte: 
+# pip install uvicorn
+#ver diretório em que vc está pwd
+#uvicorn main:app --reload
+
